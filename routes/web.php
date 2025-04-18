8d70b6b20ac4fcda01a04df2aeb0cfa5ae41be4d
BrwnsAmanda <134305327+BrwnsAmanda@users.noreply.github.com>
2025-04-18 10:30:22 +0800

    feat: add login to google

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\GoogleController;
 
 // Halaman utama
 Route::view('/', 'landing.home')->name('home');
@@ -13,3 +14,6 @@ Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 
 
+// Login Google
+Route::get('auth/google', [GoogleController::class, 'redirectToGoogle'])->name('redirect.google');
+Route::get('auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
