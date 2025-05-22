ce4c328b00de22df15fb96c11cf2ad89d55177ce
BrwnsAmanda <134305327+BrwnsAmanda@users.noreply.github.com>
2025-05-22 13:38:05 +0800

    update font beranda mitra

--- resources/views/mitra/welcome.blade.php
+++ resources/views/mitra/welcome.blade.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laboratorium Prodifa</title>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" />
     @vite('resources/css/app.css')
 </head>
-<body class="bg-cream text-gray-800 font-poppins min-h-screen flex flex-col">
+<body class="bg-white text-gray-800 font-poppins">
 
     @include('components.navbar')
 
