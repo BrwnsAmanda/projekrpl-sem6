193b421b64b1711ef186b7bb1bd7fe8677e52228
BrwnsAmanda <134305327+BrwnsAmanda@users.noreply.github.com>
2025-06-12 22:56:14 +0800

    perbaikan be rujukan admin

---
+++ app/Filament/Resources/RujukanResource/Pages/EditRujukan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\RujukanResource\Pages;
+
+use App\Filament\Resources\RujukanResource;
+use Filament\Resources\Pages\EditRecord;
+
+class EditRujukan extends EditRecord
+{
+    protected static string $resource = RujukanResource::class;
+}
---
+++ app/Filament/Resources/RujukanResource/Pages/ListRujukans.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\RujukanResource\Pages;
+
+use App\Filament\Resources\RujukanResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListRujukans extends ListRecords
+{
+    protected static string $resource = RujukanResource::class;
+}
